9e0e18185498084cf1b5b44fd6fb92cbeb3c377d
Erkmann <36168066+Erkmann@users.noreply.github.com>
2018-07-13 14:18:17 -0300

    Add files via upload

--- ip/app/views/index.php
+++ ip/app/views/index.php
@@ -3,6 +3,9 @@
 <head>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="../assets/css.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="https://v40.pingendo.com/assets/4.0.0/default/theme.css" type="text/css"> </head>
 
     <script>
         $(document).ready(function(){
@@ -29,6 +32,15 @@
     <title>Document</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-md bg-primary navbar-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">Russo e Andri</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent"> </div>
+    </div>
+</nav>
 
     <div>
         <form id="form">
@@ -37,7 +49,7 @@
             <input maxlength="3" id="3" type="text" required name="trinca3">.
             <input maxlength="3" id="4" type="text" required name="trinca4">/
             <input maxlength="2" id="masc" type="text" required name="mascara">
-            <input type="button" id="bt" value="Enviar">
+            <input class="btn-primary btn" type="button" id="bt" value="Enviar">
         </form>
     </div>
     <div id="resultado">
@@ -45,4 +57,10 @@
     </div>
 
 </body>
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </html>
